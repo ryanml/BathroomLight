c1952fd9b3aaaa3476fb6bc9309ae300527b9ed8
ryanml <rlanese@asu.edu>
2016-12-18 15:17:50 -0700

    Updates, added buttons, initial door logic.

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-var interface = require("johnny-five");
-var board = new interface.Board();
+var interf = require("johnny-five");
+var board = new interf.Board();
 
 board.on("ready", function() {
 
+    var start, end;
+    var doorOpen = true;
+    const coolDown = 10000;
+
     const pinLayout = {
         pins: {
-            red: 3, 
-            green: 5,
-            blue: 6
+            red: 11, 
+            green: 10,
+            blue: 9
         }
     };
+
+    const turnAllOff = (leds) => {
+        leds.map(led => led.off());
+    }
 
     const randomHex = () => {
         var hex = '#';
@@ -24,12 +32,46 @@ board.on("ready", function() {
         return hex;
     };
 
-    var light = new interface.Led.RGB(pinLayout);
+    var leds {
+        yellow: new interf.Led(3),
+        green: new interf.Led(4),
+        red: new interf.Led(5)
+    };
 
-    light.on();
-    setInterval(function() {
-        var hex = randomHex();
-        light.color(hex);
-        light.blink(2000);
-    }, 4000);
+    var ledButton = new interf.Button(2);
+    var rgbButton = new interf.Button(13);
+
+    var rgbLed = new interf.Led.RGB(pinLayout);
+    var rgbState = false;
+
+    ledButton.on('down', () => {
+        turnAllOff();
+        if (doorOpen) {
+            doorOpen = false;
+            start = new Date();
+            leds.red.on();
+        } else {
+            doorOpen = true;
+            end = new Date();
+            if ((end - start) > coolDown) {
+                leds.yellow.on();
+            } else {
+                leds.green.on();
+            }
+        }
+    });
+    
+    rgbButton.on('down', () => {
+        if (!rgbState) {
+            rgbLed.color(
+                randomHex()
+            );
+            rgbLed.on();
+            rgbState = true;
+        } else {
+            rgbLed.off();
+            rgbState = false;
+        }
+    });
+
 });
